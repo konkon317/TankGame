e2339fbc4d9aa3b86687f2034a42564177c5b7af
藤井康輔　win7_deskTop <konkon.317@gmail.com>
2015-02-09 22:34:35 +0900

    カメラが戦車を追従するようになる

---
+++ Assets/Project/Script/cameraMoveMent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMoveMent : MonoBehaviour 
+{
+	[SerializeField]
+	Transform targetObject;
+
+	Vector3 relPos;
+	// Use this for initialization
+	void Start () 
+	{
+		relPos = this.transform.position - targetObject.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.position = targetObject.position + relPos;
+	}
+}

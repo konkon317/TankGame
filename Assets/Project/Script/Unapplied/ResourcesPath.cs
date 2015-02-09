24061eb00288450702df28b62c87eed1e98c2d6d
藤井康輔　win7_deskTop <konkon.317@gmail.com>
2015-02-09 23:09:36 +0900

    壁（普通）と壁（弱点）のプリファブ 壁をランダム生成するスクリプト

--- Assets/Project/Script/Unapplied/ResourcesPath.cs
+++ Assets/Project/Script/Unapplied/ResourcesPath.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class ResourcesPath : MonoBehaviour 
 {
 	public const string Prefab_Bullet ="Prefab/Bullet";
+	public const string Prefab_WallBlcok = "Prefab/wall/WallBlock";
+	public const string Prefab_WallBlockWeak = "Prefab/wall/WallBlockWeak";
 
 	
 }
---
+++ Assets/WallCreater.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCreater : MonoBehaviour 
+{
+	const int WallMax=15;
+	const int WeakPointRange = 7;
+
+	GameObject WallBlcok_Prefab;
+	GameObject WallBlockWeak_Prefab;
+
+	void Awake()
+	{
+		WallBlcok_Prefab = Resources.Load<GameObject>(ResourcesPath.Prefab_WallBlcok);
+		WallBlockWeak_Prefab = Resources.Load<GameObject>(ResourcesPath.Prefab_WallBlockWeak);
+	
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		int rand = Random.Range(0, WeakPointRange);
+
+		for (int i = 0; i < WallMax; i++)
+		{
+			GameObject prefab;
+			if (i == rand)
+			{
+				prefab = WallBlockWeak_Prefab;
+			}
+			else
+			{
+				prefab = WallBlcok_Prefab;
+			}
+
+			GameObject obj = (GameObject)Instantiate(prefab);
+			obj.transform.position = this.transform.position + (new Vector3(0, i, 0));
+
+			obj.transform.parent = transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}

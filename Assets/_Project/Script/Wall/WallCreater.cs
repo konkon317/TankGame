cafab2a091dd50e56cba9f1e188bf4aaaebba6fb
藤井康輔　win7_deskTop <konkon.317@gmail.com>
2015-02-11 14:52:56 +0900

    エフェクトのアセットを追加

---
+++ Assets/_Project/Script/Wall/WallCreater.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCreater : MonoBehaviour 
+{
+	const int WallMax=15;
+	const int WeakPointRange = 7;
+
+	GameObject WallBlcok_Prefab;
+	GameObject WallBlockWeak_Prefab;
+
+	bool isCreated = false;
+
+	void Awake()
+	{
+		WallBlcok_Prefab = Resources.Load<GameObject>(ResourcesPath.Prefab_WallBlcok);
+		WallBlockWeak_Prefab = Resources.Load<GameObject>(ResourcesPath.Prefab_WallBlockWeak);
+	
+	}
+
+	
+	void Start () 
+	{
+		Create();
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+
+	/// <summary>
+	/// 壁を作成します
+	/// </summary>
+	void Create()
+	{
+		if (!isCreated)
+		{
+			int rand = Random.Range(0, WeakPointRange);
+
+			for (int i = 0; i < WallMax; i++)
+			{
+				GameObject prefab;
+				if (i == rand)
+				{
+					prefab = WallBlockWeak_Prefab;
+				}
+				else
+				{
+					prefab = WallBlcok_Prefab;
+				}
+
+				GameObject obj = (GameObject)Instantiate(prefab);
+				obj.transform.position = this.transform.position + (new Vector3(0, i, 0));
+
+				obj.transform.parent = transform;
+			}
+			isCreated = true;
+		}
+	}
+}
---
+++ Assets/_Project/Script/cameraMoveMent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// カメラに付けるコンポーネント
+/// targetObjectとの距離を一定に保つように移動する
+/// </summary>
+public class cameraMoveMent : MonoBehaviour 
+{
+	
+	[SerializeField]
+	Transform targetObject;
+
+	Vector3 relPos;
+	// Use this for initialization
+	void Start () 
+	{
+		relPos = this.transform.position - targetObject.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.position = targetObject.position + relPos;
+	}
+}

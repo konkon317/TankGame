d17c8609184d1c8e7edbe528de70056e1599eff2
KousukeFujii macBook <konkon317@gmail.com>
2015-02-13 18:58:44 +0900

    砲弾が物に当たると爆発して消えるようになる

---
+++ Assets/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour 
+{
+    [SerializeField]
+    GameObject detonatorPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string tag = collision.collider.tag;
+
+        if (tag == Tags.WallBlockCube || tag == Tags.Ground)
+        {
+            GameObject obj = (GameObject)Instantiate(detonatorPrefab);
+            obj.transform.position = this.transform.position;
+
+            Destroy(this.gameObject);
+
+        }
+        else
+        {
+            if (tag == Tags.WallBlockCubeWeak)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
--- Assets/_Project/Script/Unapplied/Tags.cs
+++ Assets/_Project/Script/Unapplied/Tags.cs
@@ -6,5 +6,9 @@ public class Tags : MonoBehaviour
 	public const string TankMuzzle = "TankMuzzle";
 	public const string BulletsParent = "Bullets";
     public const string Bullet="Bullet";
+    public const string Tank="Tank";
+    public const string Ground="Ground";
+    public const string WallBlockCube="WallBlockCube";
+    public const string WallBlockCubeWeak="WallBlockCubeWeak";
 	
 }

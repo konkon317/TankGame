13445ce119ebc28b0a7ea6d919ed52ddf5b9b6f4
藤井康輔　win7_deskTop <konkon.317@gmail.com>
2015-03-02 02:41:34 +0900

    チュートリアルシーンのクリア条件の判断が完成

---
+++ Assets/TutorialWall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialWall : MonoBehaviour 
+{
+	GameController gameController;
+	WallParent wall;
+	void Awake()
+	{
+		gameController = GameObject.FindWithTag(Tags.GameController).GetComponent<GameController>();
+		wall = GetComponent<WallParent>();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (wall.Crashed == true)
+		{
+			gameController.SetGameOverFlag();
+		}
+	}
+}
